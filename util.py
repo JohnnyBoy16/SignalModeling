a06db35a10aa0b66a1abe5948a4bf48c26f51a56
John Nagel <jnagel@iastate.edu>
2017-11-30 22:25:15 -0600

    backing up, next step add initial guess to curve_fit

---
+++ util.py
@@ -0,0 +1,19 @@
+
+
+def parabolic_equation(data, a, b, c, d, e):
+    """
+    Equation for a paraboloid to fit to the cost function for determining the index of refraction
+    of a material
+    :param data:
+    :param a:
+    :param b:
+    :param c:
+    :param d:
+    :param e:
+    :return:
+    """
+    x, y = data
+
+    paraboloid = ((x - a) / b) ** 2 + ((y - d) / e) ** 2 + c
+
+    return paraboloid
