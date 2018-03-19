39a5bf994b82bdfa2a271b247e4a96e26b01635f
John Nagel <jnagel@iastate.edu>
2018-03-18 22:48:55 -0500

    changed to abolute imports

--- test.py
+++ test.py
@@ -1,117 +1,47 @@
-"""
-Test script to check index of refraction solution be using the forward model
-"""
-import copy
-import socket
-import sys
-
-import numpy as np
 import matplotlib.pyplot as plt
-import wx
+import numpy as np
 
-import half_space as hs
-import sm_functions as sm
+from THzProc.THzData import RefData
+from base_util import signal_model_functions as sm
 
-if socket.gethostname() == 'Laptop':
-    drive = 'C:'
-else:
-    drive = 'D:'
+filename = 'C:\\Work\\Refs\\ref 21FEB2018\\40ps waveform.txt'
 
-sys.path.insert(0, drive + '\\PycharmProjects\\THzProcClass')
+gate = [500, 1250]
 
-from THzData import THzData
-from FrameHolder import FrameHolder
+n_layers = 1
 
-ref_file = drive + '\\Work\\Refs\\ref 18OCT2017\\30ps waveform.txt'
+d = np.array([0])
 
-basedir = drive + '\\Work\\Shim Stock\\New Scans'
-tvl_file = 'Yellow Shim Stock.tvl'
+ref = RefData(filename, gate=gate)
 
-# index of refraction value that we want to check
-n0 = complex(1.64, -0.04)
-
-# thickness of the sample
-d = np.array([0.508])
-
-gate0 = 450  # index to remove front "blip"
-gate1 = 2050  # 2nd gate for reference signal, this cuts out on water vapor lines
+e0 = -ref.freq_waveform
 
 theta0 = 17.5 * np.pi / 180
 
-ref_time, ref_amp = sm.read_reference_data(ref_file)
+n = np.array([1, np.inf])
 
-data = THzData(tvl_file, basedir)
+c = 0.2998
 
-ref_freq_amp = copy.deepcopy(ref_amp)
-ref_freq_amp[:gate0] = 0
-ref_freq_amp[gate1:] = 0
+theta = np.zeros(2, dtype=complex)
+theta[0] = theta0
+theta[1] = 0
 
-# determine the wave length that was gathered
-# calculate dt and df
-wave_length = len(ref_time)
-dt = ref_time[1]
-df = 1 / (wave_length * dt)
+r = np.zeros(2, dtype=complex)
+r[1] = sm.reflection_coefficient(n[0], n[1], theta[0], theta[1])
 
-ref_freq = np.linspace(0., (wave_length/2) * df, wave_length//2+1)
+delta = 2 * np.pi * ref.freq/c * d[0] * n[0] * np.cos(theta[0])
+z = np.exp(-2j * delta)
 
-ref_freq_amp = np.fft.rfft(ref_freq_amp) * dt
-e0 = -ref_freq_amp  # initial E0 is the opposite of reference
+gamma = (r[1]+r[0] * z) / (1+r[1]*r[0] * z)
 
-plt.figure('Reference Data')
-plt.subplot(211)
-plt.plot(ref_time, ref_amp, 'r')
-plt.axvline(ref_time[gate0], color='k', linestyle='--')
-plt.axvline(ref_time[gate1], color='k', linestyle='--')
-plt.xlabel('Time (ps)')
-plt.ylabel('Amplitude')
-plt.grid(True)
+E2 = gamma * e0
 
-plt.subplot(212)
-plt.plot(ref_freq, np.abs(ref_freq_amp), 'r')
-plt.xlabel('Frequency (THz)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.xlim(0, 3.5)
-
-# build the half space model and add the
-theta1 = sm.get_theta_out(1.0, n0, theta0)
-
-FSE = hs.half_space_model(e0, ref_freq, n0, 0, theta0, theta1)
-BSE = hs.half_space_model(e0, ref_freq, n0, d, theta0, theta1)
-
-signal = FSE + BSE
-
-time_signal = np.fft.irfft(signal) / dt
+e2 = np.fft.irfft(E2) / ref.dt
 
 plt.figure('Return Signal')
-plt.plot(ref_time, time_signal, 'r')
+plt.plot(ref.time, e2, 'r')
 plt.xlabel('Time (ps)')
 plt.ylabel('Amplitude')
-plt.grid(True)
-
-# build a global reflection model to see if they are the same
-theta = np.zeros(3, dtype=complex)
-theta[0] = theta0
-theta[-1] = theta0
-theta[1] = theta1
-
-n = np.ones(3, dtype=complex)
-n[1] = n0
-gamma = sm.global_reflection_model(n, theta, data.freq, d, n_layers=1)
-
-signal = gamma[0] * e0
-time_signal = np.fft.irfft(signal) / dt
-
-plt.figure('Return Signal from Global Model')
-plt.plot(ref_time, time_signal, 'r')
-plt.xlabel('Time (ps)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-
-app = wx.App(False)
-
-holder = FrameHolder(data)
+plt.grid()
 
 plt.show()
-
-app.MainLoop()
